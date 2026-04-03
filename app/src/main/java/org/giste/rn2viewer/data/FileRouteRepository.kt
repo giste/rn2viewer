c5e27ba1d4247a8a57bb7b33e19fbbdc9310ff67
Giste <giste.trappiste@gmail.com>
2026-04-03 13:25:42 +0200

    Add GPL license

--- app/src/main/java/org/giste/rn2viewer/data/FileRouteRepository.kt
+++ app/src/main/java/org/giste/rn2viewer/data/FileRouteRepository.kt
@@ -1,3 +1,21 @@
+/*
+ * Rn2 Viewer
+ * Copyright (C) 2024  Giste
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.giste.rn2viewer.data
 
 import android.content.Context
@@ -7,7 +25,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.giste.rn2viewer.domain.model.Route
 import org.giste.rn2viewer.domain.repositories.RouteRepository
