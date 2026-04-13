d387099fbbc4c2a25be475555ca32d4f16e3196f
Giste <giste.trappiste@gmail.com>
2026-04-13 17:21:54 +0200

    Add MaterialComponents and change icon to use android colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.google.material)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.hilt.android)
     implementation(libs.hilt.compiler)
