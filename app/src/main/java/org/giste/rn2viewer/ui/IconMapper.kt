592b5a7284b0d540565e4c0a91da18a8fc34cac4
Giste <giste.trappiste@gmail.com>
2026-04-13 15:15:09 +0200

    Add missing icon uuids

--- app/src/main/java/org/giste/rn2viewer/ui/IconMapper.kt
+++ app/src/main/java/org/giste/rn2viewer/ui/IconMapper.kt
@@ -24,18 +24,18 @@ object IconMapper {
     private const val CROSS_DANGER_1_ID = "bffeadbd-116b-49a7-921e-20dff8deec4b"
     private const val CROSS_DANGER_2_ID = "a6c80c12-49b1-4e68-a21f-a6d48ef0a0ed"
     private const val CROSS_DANGER_3_ID = "fab72ac2-f809-4ddc-9a7a-c9a24768bb4e"
-    private const val CROSS_FUEL_ZONE_ID = "Unknown_03"
+    private const val CROSS_FUEL_ZONE_ID = "e5167bd4-314b-47d3-ba23-708182be76a9"
     private const val CROSS_RESET_DISTANCE_ID = "308c7365-bc3f-451b-9e98-531e9015024f"
-    private const val LANDMARK_ABOVE_BRIDGE_ID = "Unknown_07"
-    private const val LANDMARK_FORT_CASTLE_1_ID = "Unknown_09"
-    private const val LANDMARK_HOUSE_ID = "Unknown_02"
-    private const val LANDMARK_TRAFFIC_LIGHT_ID = "Unknown_10"
-    private const val LANDMARK_TUNNEL_ID = "Unknown_04"
-    private const val LANDMARK_UNDER_BRIDGE_ID = "Unknown_08"
-    private const val SIGN_ALERT_ID = "Unknown_11"
-    private const val SIGN_ROUNDABOUT_ID = "Unknown_05"
-    private const val SIGN_STOP_ID = "Unknown_01"
-    private const val TERRAIN_RIVER_WATER_ID = "Unknown_06"
+    private const val LANDMARK_ABOVE_BRIDGE_ID = "a49a0b2e-3be5-4659-8251-8205fd4e9571"
+    private const val LANDMARK_FORT_CASTLE_ID = "da5ec2a7-612a-411f-aeb2-d1f9514d3dc7"
+    private const val LANDMARK_HOUSE_ID = "3965bf45-97ee-4c6b-b087-0e128510c4e3"
+    private const val LANDMARK_TRAFFIC_LIGHT_ID = "1d752896-09fd-498d-b416-21f31a356be5"
+    private const val LANDMARK_TUNNEL_ID = "0539c8e3-393b-4416-8002-b30700cf68de"
+    private const val LANDMARK_UNDER_BRIDGE_ID = "79f8c10f-d67b-4ba5-bf12-6a801ed79ed3"
+    private const val SIGN_ALERT_ID = "2598a2c0-6a8b-4dc5-8211-8ad64d986bde"
+    private const val SIGN_ROUNDABOUT_ID = "5d157992-6013-4bef-86cb-92fea891944c"
+    private const val SIGN_STOP_ID = "5a4ced4c-68e2-41d3-a1b4-9c8b86ec2109"
+    private const val TERRAIN_RIVER_WATER_ID = "aabe9acd-ab1b-467d-9bbb-877bb0d0da23"
 
     fun getDrawableId(iconId: String): Int {
         return when (iconId) {
@@ -45,7 +45,7 @@ object IconMapper {
             CROSS_FUEL_ZONE_ID -> R.drawable.ic_cross_fuel_zone
             CROSS_RESET_DISTANCE_ID -> R.drawable.ic_cross_reset_distance
             LANDMARK_ABOVE_BRIDGE_ID -> R.drawable.ic_landmark_above_bridge
-            LANDMARK_FORT_CASTLE_1_ID -> R.drawable.ic_landmark_fort_castle_1
+            LANDMARK_FORT_CASTLE_ID -> R.drawable.ic_landmark_fort_castle_1
             LANDMARK_HOUSE_ID -> R.drawable.ic_landmark_house
             LANDMARK_TRAFFIC_LIGHT_ID -> R.drawable.ic_landmark_traffic_light
             LANDMARK_TUNNEL_ID -> R.drawable.ic_landmark_tunnel
