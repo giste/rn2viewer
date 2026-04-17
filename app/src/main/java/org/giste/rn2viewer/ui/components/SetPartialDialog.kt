41a75d49fe4174e49b7aa772ed51c911f06ed851
Giste <giste.trappiste@gmail.com>
2026-04-17 08:58:41 +0200

    Maintain value while rotating

--- app/src/main/java/org/giste/rn2viewer/ui/components/SetPartialDialog.kt
+++ app/src/main/java/org/giste/rn2viewer/ui/components/SetPartialDialog.kt
@@ -46,7 +46,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -69,7 +69,7 @@ fun SetPartialDialog(
     onDismiss: () -> Unit,
     onConfirm: (Double) -> Unit
 ) {
-    var input by remember { mutableStateOf("") }
+    var input by rememberSaveable { mutableStateOf("") }
     val isLandscape = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
 
     Dialog(
